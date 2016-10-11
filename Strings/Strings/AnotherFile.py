7952d41240956ad0bb43c6273724d72f6a401704
Eric Charnesky <echarnes@UMICH.EDU>
2016-10-11 17:20:26 -0400

    string examples

---
+++ Strings/Strings/AnotherFile.py
@@ -0,0 +1,7 @@
+hello = ' another hello world hello hello hello hi there '
+numberOfHellos = hello.count('h')
+print ( numberOfHellos )
+
+print( hello.split() )
+print( len(hello.split() ) )
+print( ' '.join(hello.split()) )
