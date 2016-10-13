309534a0bf4e4f5506dab42f9bee0e25caca0a3b
Eric Charnesky <eric.charnesky@careevolution.com>
2016-10-13 17:19:30 -0400

    Finished lab 6-19

--- Strings/Strings/Lab6.py
+++ Strings/Strings/Lab6.py
@@ -1,8 +1,39 @@
 # Type all other functions here
 
+def shorten_space(usrStr):
+    split = usrStr.split()
+    return ( " ".join(split) )
 
+def replace_punctuation(usrStr):
+    return usrStr.count("!"), usrStr.count(";"), usrStr.replace("!",".").replace(";",",")
+    
+def fix_capitilization(usrStr):
+    numberOfSentencesCapitalized = 0
+
+    sentences = usrStr.split('.')
+    # looping through sentence list so we can modify the sentence in the collection
+    for number in range(len(sentences ) ):
+        sentence = sentences[number]
+        # don't process the empty string if sentence ends with .
+        if len( sentence ) > 0:
+            # loops throug string so we can find the first non space
+            for index in range(len(sentence) ):
+                if not sentence[index].isspace():
+                    # first non space character, fix or not
+                    if not sentence[index].isupper():
+                        sentences[number] = sentence[:index] + sentence[index].upper() + sentence[index + 1:]
+                        numberOfSentencesCapitalized += 1
+                    #break out of the range loop so we don't capitialize anything else
+                    break
+    capitalizedString = ".".join(sentences)
+
+    return numberOfSentencesCapitalized, capitalizedString
+
+def get_num_of_words(usrStr):
+    return len( usrStr.split() )
 
 def print_menu(usrStr):
+   print( 'You entered:', usrStr )
    validOptions = ( 'c', 'w', 'f', 'r', 's', 'q' )
    menuOp = ' '
 
@@ -21,13 +52,13 @@ def print_menu(usrStr):
        if menuOp == 'c':
            print('Number of non-whitespace characters: %d' % get_num_of_non_WS_characters(usrStr) )
        elif menuOp == 'w':
-           print( 'FIX ME')
+           print( 'Number of words: %d' % get_num_of_words(usrStr) )
        elif menuOp == 'f':
-            print( 'FIX ME')
+           print( 'Number of letters capitalized: %d\nEdited text: %s' % fix_capitilization(usrStr) )
        elif menuOp == 'r':
-           print( 'FIX ME')
+           print( 'Punctuation replaced\nexclamationCount: %d\nsemicolonCount: %d\nEdited text: %s' % replace_punctuation(usrStr) )
        elif menuOp == 's':
-            print( 'FIX ME')
+            print( 'Edited text: %s' % shorten_space(usrStr) )
 
    return menuOp, usrStr
 
